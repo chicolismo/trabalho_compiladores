b23a97392f16ef8bd08fc71478468a4a079b154f
Unknown <marcelo.andrighetto@hotmail.com>
2017-12-18 21:16:04 -0200

    Logic operations asm

--- etapa6/asm.c
+++ etapa6/asm.c
@@ -23,13 +23,15 @@ void generate_temp(TAC *tac) {
     
 }
 
-void generate_math_operation(TAC *tac) {
+void generate_math_or_logic_operation(TAC *tac) {
     char operation[6] = "\0";
     switch (tac->type) {
         case TAC_ADD: strcpy(operation, "addl"); break;
         case TAC_SUB: strcpy(operation, "subl"); break;
         case TAC_MUL: strcpy(operation, "imull"); break;
         case TAC_DIV: strcpy(operation, "idivl"); break;
+        case TAC_AND: strcpy(operation, "andl"); break;
+        case TAC_OR: strcpy(operation, "orl"); break;
         default: return;
     }
     
@@ -57,10 +59,6 @@ void generate_compare_operation(TAC *tac) {
     fprintf(output_file, "\tandb\t$1, %%dl\n");
     fprintf(output_file, "\tmovzbl\t%%dl, %%ecx\n");
     fprintf(output_file, "\tmovl\t%%ecx, %s(%%rip)\n", tac->res->string);
-}
-
-void generate_logic_operation(TAC *tac) {
-    
 }
 
 void generate_not(TAC *tac) {
@@ -141,16 +139,15 @@ void generate_asm(TAC *tac_list) {
 
         while(scan != NULL) {
             switch(scan->type) {
+                case SYMBOL_LIT_INTEGER:
+                case SYMBOL_LIT_REAL:
+                case SYMBOL_LIT_CHAR:
+                case SYMBOL_STRING:
+                case SYMBOL_IDENTIFIER_SCALAR:
+                    generate_scalar_var(scan); break;
 
-            case SYMBOL_LIT_INTEGER:
-            case SYMBOL_LIT_REAL:
-            case SYMBOL_LIT_CHAR:
-            case SYMBOL_STRING:
-            case SYMBOL_IDENTIFIER_SCALAR:
-                generate_scalar_var(scan); break;
-
-            case SYMBOL_IDENTIFIER_VECTOR:
-                generate_vector_var(scan); break;
+                case SYMBOL_IDENTIFIER_VECTOR:
+                    generate_vector_var(scan); break;
             }
 
             scan = scan->next;
@@ -163,78 +160,75 @@ void generate_asm(TAC *tac_list) {
     
     while (tac) {
         switch (tac->type) {
+            case TAC_SYMBOL:
+                break;
 
-        case TAC_SYMBOL:
-            break;
+            case TAC_LABEL:
+                generate_label(tac); break;
 
-        case TAC_LABEL:
-            generate_label(tac); break;
+            case TAC_TEMP:
+                generate_temp(tac); break;
 
-        case TAC_TEMP:
-            generate_temp(tac); break;
+            case TAC_ADD:
+            case TAC_SUB:
+            case TAC_MUL:
+            case TAC_DIV:
+            case TAC_AND:
+            case TAC_OR:
+                generate_math_or_logic_operation(tac); break;
 
-        case TAC_ADD:
-        case TAC_SUB:
-        case TAC_MUL:
-        case TAC_DIV:
-            generate_math_operation(tac); break;
+            case TAC_LT:
+            case TAC_GT:
+            case TAC_LE:
+            case TAC_GE:
+            case TAC_EQ:
+            case TAC_NE:
+                generate_compare_operation(tac); break;
 
-        case TAC_LT:
-        case TAC_GT:
-        case TAC_LE:
-        case TAC_GE:
-        case TAC_EQ:
-        case TAC_NE:
-            generate_compare_operation(tac); break;
+            case TAC_NOT:
+                generate_not(tac); break;
 
-        case TAC_AND:
-        case TAC_OR:
-            generate_logic_operation(tac); break;
+            case TAC_READ:
+                generate_read(tac); break;
 
-        case TAC_NOT:
-            generate_not(tac); break;
+            case TAC_PRINT:
+                generate_print(tac); break;
 
-        case TAC_READ:
-            generate_read(tac); break;
+            case TAC_RET:
+                generate_return(tac); break;
 
-        case TAC_PRINT:
-            generate_print(tac); break;
+            case TAC_JZ:
+                generate_jz(tac); break;
 
-        case TAC_RET:
-            generate_return(tac); break;
+            case TAC_JMP:
+                generate_jump(tac); break;
 
-        case TAC_JZ:
-            generate_jz(tac); break;
+            case TAC_ASSIGN:
+                generate_assign(tac); break;
 
-        case TAC_JMP:
-            generate_jump(tac); break;
+            case TAC_ARRAY_ASSIGN:
+                generate_array_assign(tac); break;
 
-        case TAC_ASSIGN:
-            generate_assign(tac); break;
+            case TAC_ARRAY_INDEX:
+                generate_array_index(tac); break;
 
-        case TAC_ARRAY_ASSIGN:
-            generate_array_assign(tac); break;
+            case TAC_BEGIN_FUNC:
+                generate_begin_func(tac); break;
 
-        case TAC_ARRAY_INDEX:
-            generate_array_index(tac); break;
+            case TAC_END_FUNC:
+                generate_end_func(tac); break;
 
-        case TAC_BEGIN_FUNC:
-            generate_begin_func(tac); break;
+            case TAC_CALL:
+                generate_call(tac); break;
 
-        case TAC_END_FUNC:
-            generate_end_func(tac); break;
+            case TAC_PUSH_ARG:
+                generate_push_arg(tac); break;
 
-        case TAC_CALL:
-            generate_call(tac); break;
+            case TAC_POP_ARG:
+                generate_pop_arg(tac); break;
 
-        case TAC_PUSH_ARG:
-            generate_push_arg(tac); break;
-
-        case TAC_POP_ARG:
-            generate_pop_arg(tac); break;
-
-        default:
-            break;
+            default:
+                break;
         }
         
         tac = tac->next;
